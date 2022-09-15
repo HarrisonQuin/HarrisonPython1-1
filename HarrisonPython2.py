860bab2bc2ac301c402227f57286d5faf0da8a48
Harrison <harrisonquinn619@gmail.com>
2022-09-15 11:58:51 -0400

    Initial commit

---
+++ HarrisonPython2.py
@@ -0,0 +1,8 @@
+nameset = input("Whats your name? ")
+print("What up " + nameset)
+major = input("Whats your major? ")
+print(f"Thats cool {nameset}, good thing comp101 will help you with your {major} degree! Good Luck!")
+
+
+loud_class = nameset.upper()
+print(loud_class)
